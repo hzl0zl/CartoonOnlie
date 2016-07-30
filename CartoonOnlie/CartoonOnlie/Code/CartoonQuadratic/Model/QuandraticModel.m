8097c4caf7d27a8e67271a84cb1a5413eb5a37c5
zhiling <zhiling@laosijidemac.local>
2016-07-30 11:04:17 +0800

    QuandraticModel

---
+++ CartoonOnlie/CartoonOnlie/Code/CartoonQuadratic/Model/QuandraticModel.m
@@ -0,0 +1,13 @@
+//
+//  QuandraticModel.m
+//  CartoonOnlie
+//
+//  Created by zhiling on 16/7/30.
+//  Copyright © 2016年 huangzhiling. All rights reserved.
+//
+
+#import "QuandraticModel.h"
+
+@implementation QuandraticModel
+
+@end
