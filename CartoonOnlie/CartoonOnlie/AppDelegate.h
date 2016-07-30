70970dffbce6361caa942fe5f1b96658cfcc31a6
zhiling <zhiling@laosijidemac.local>
2016-07-30 09:00:11 +0800

    初始化

---
+++ CartoonOnlie/CartoonOnlie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CartoonOnlie
+//
+//  Created by zhiling on 16/7/30.
+//  Copyright © 2016年 huangzhiling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CartoonOnlie/CartoonOnlie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CartoonOnlie
+//
+//  Created by zhiling on 16/7/30.
+//  Copyright © 2016年 huangzhiling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
