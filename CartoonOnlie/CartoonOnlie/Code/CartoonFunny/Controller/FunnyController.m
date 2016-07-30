630a3638dff35f8f05c8a070cebcfb02164aa905
lanou <lan@heqikangdemac-mini.local>
2016-07-30 10:59:18 +0800

    first commit

--- CartoonOnlie/CartoonOnlie/Code/CartoonFunny/Controller/FunnyController.m
+++ CartoonOnlie/CartoonOnlie/Code/CartoonFunny/Controller/FunnyController.m
@@ -8,30 +8,150 @@
 
 #import "FunnyController.h"
 
-@interface FunnyController ()
+
+
+@interface FunnyController ()<UITableViewDataSource, UITableViewDelegate>
+
+
+
+@property (nonatomic, strong) UITableView *tableView;
+
+
+
+@property (nonatomic, strong) NSMutableArray *funList;
+
+
 
 @end
 
+
+
 @implementation FunnyController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
+
+
+#pragma mark -- 懒加载
+
+- (NSMutableArray *)funList
+
+{
+    
+    if (_funList == nil) {
+        
+        _funList = [NSMutableArray array];
+        
+    }
+    
+    return _funList;
+    
 }
+
+
+
+- (UITableView *)tableView
+
+{
+    
+    if (_tableView == nil) {
+        
+        _tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:0];
+        
+        _tableView.delegate = self;
+        
+        _tableView.dataSource = self;
+        
+        [self.view addSubview:_tableView];
+        
+    }
+    
+    return _tableView;
+    
+}
+
+
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+
+{
+    
+    return self.funList.count;
+    
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+
+{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"funListCell"];
+    
+    
+    
+    
+    
+    return cell;
+    
+}
+
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+
+{
+    
+    return 80.f;
+    
+}
+
+
+
+
+
+
+
+
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
+    
     [super didReceiveMemoryWarning];
+    
     // Dispose of any resources that can be recreated.
+    
 }
+
+
 
 /*
-#pragma mark - Navigation
+ 
+ #pragma mark - Navigation
+ 
+ 
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ 
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ 
+ // Get the new view controller using [segue destinationViewController].
+ 
+ // Pass the selected object to the new view controller.
+ 
+ }
+ 
+ */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
