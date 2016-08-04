8631a3953b758aff11eff7de2f403ba01bdbf4d2
lanou <lan@heqikangdemac-mini.local>
2016-08-04 17:14:48 +0800

    完成第一个页面

---
+++ CartoonOnlie/CartoonOnlie/Code/CartoonFunny/Views/VolumecountCell.h
@@ -0,0 +1,16 @@
+//
+//  VolumecountCell.h
+//  CartoonOnlie
+//
+//  Created by lanou on 16/8/4.
+//  Copyright © 2016年 huangzhiling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VolumecountCell : UICollectionViewCell
+
+@property (strong, nonatomic) IBOutlet UIButton *volumecountBtn;
+
+
+@end
---
+++ CartoonOnlie/CartoonOnlie/Code/CartoonFunny/Views/VolumecountCell.m
@@ -0,0 +1,17 @@
+//
+//  VolumecountCell.m
+//  CartoonOnlie
+//
+//  Created by lanou on 16/8/4.
+//  Copyright © 2016年 huangzhiling. All rights reserved.
+//
+
+#import "VolumecountCell.h"
+
+@implementation VolumecountCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
